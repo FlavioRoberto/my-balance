6207097c32bbab74b2c0ef6113671a98906fba28
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2022-06-17 17:38:59 -0300

    chore: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main/index.tsx',
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        publicPath: '/public/js',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
+        alias: {
+            '@': path.join(__dirname, 'src')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(s?)css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    devServer: {
+        static: './public',
+        historyApiFallback: true,
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
