1c3af190e82249e8f956edf1925e640c39c08698
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2022-06-20 21:32:02 -0300

    refactor: "ajusted graph-column methods"

--- src/presentation/components/graph/graph-column.tsx
+++ src/presentation/components/graph/graph-column.tsx
@@ -3,23 +3,28 @@ import { Badge } from "@/presentation/components";
 import { getPercent } from "@/domain";
 import './graph-column.scss';
 
-interface GraphColumn {
+interface Props {
     spendValue: number,
     maxValue: number,
-    description: string
+    description: string,
 }
 
 function getColumnStyle(value, maxValue): React.CSSProperties {
-    const style = { height: `${getPercent(value, maxValue)}%` } as React.CSSProperties;
-
-    if (value === maxValue)
-        style.backgroundColor = "#B5DFE5";
-
-    return style;
+    return { height: `${getPercent(value, maxValue)}%` } as React.CSSProperties;
 }
 
-const GraphColumn: React.FC<GraphColumn> = (props: GraphColumn) => {
+function getColumnClass(principalColumn: boolean): string | undefined {
+    const columnClass = "column";
+
+    if (principalColumn)
+        return `${columnClass} principal`;
+
+    return columnClass;
+}
+
+const GraphColumn: React.FC<Props> = (props: Props) => {
     const [showBadge, setShowBadge] = useState(false);
+    const isPrincipalColumn = props.spendValue === props.maxValue;
 
     const onSetShowBadge = (enable: boolean) => {
         setShowBadge(enable)
@@ -27,7 +32,7 @@ const GraphColumn: React.FC<GraphColumn> = (props: GraphColumn) => {
 
     return <div className="graph-column" onMouseEnter={() => onSetShowBadge(true)} onMouseLeave={() => onSetShowBadge(false)}>
         {showBadge && <Badge description={props.spendValue.toFixed(2)} />}
-        <div className="column" style={getColumnStyle(props.maxValue, props.spendValue)}></div>
+        <div className={getColumnClass(isPrincipalColumn)} style={getColumnStyle(props.maxValue, props.spendValue)}></div>
         <span className="description">{props.description}</span>
     </div>
 }
